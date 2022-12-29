53d87358696b0733d4093383b430b8abef986201
YiGuMoYan <3194775246@qq.com>
2022-12-29 22:41:38 +0800

    fixed

--- src/main/java/top/yigumoyan/blogjava/controller/AboutController.java
+++ src/main/java/top/yigumoyan/blogjava/controller/AboutController.java
@@ -2,10 +2,7 @@ package top.yigumoyan.blogjava.controller;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import top.yigumoyan.blogjava.entity.About;
 import top.yigumoyan.blogjava.result.Result;
 import top.yigumoyan.blogjava.service.Impl.AboutServiceImpl;
@@ -24,34 +21,35 @@ public class AboutController {
     private AboutServiceImpl aboutService;
 
     @PostMapping("/insertAbout")
-    public Result insertAbout(About about) {
+    public Result insertAbout(@RequestBody About about) {
         aboutService.insertAbout(about);
         return Result.ok(true);
     }
 
     @PostMapping("/deleteAboutById")
-    public Result deleteAboutById(int id) {
+    public Result deleteAboutById(@RequestBody int id) {
         aboutService.deleteAboutById(id);
         return Result.ok(true);
     }
 
     @PostMapping("/updateAbout")
-    public Result updateAbout(About about) {
+    public Result updateAbout(@RequestBody About about) {
+        System.out.println(about);
         aboutService.updateAbout(about);
         return Result.ok(true);
     }
 
-    @PostMapping("/selectAllAbout")
+    @GetMapping("/selectAllAbout")
     public Result selectAllAbout() {
         return Result.ok(aboutService.selectAllAbout());
     }
 
     @PostMapping("/selectAboutById")
-    public Result selectAboutById(int id) {
+    public Result selectAboutById(@RequestBody int id) {
         return Result.ok(aboutService.selectAboutById(id));
     }
 
-    @PostMapping("/selectOneAbout")
+    @GetMapping("/selectOneAbout")
     public Result selectOneAbout() {
         return Result.ok(aboutService.selectAllAbout().get(0));
     }
