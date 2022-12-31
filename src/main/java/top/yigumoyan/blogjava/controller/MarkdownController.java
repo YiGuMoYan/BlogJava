b51490f79e0ea5a14cd1b3930db8aee9d6699832
YiGuMoYan <3194775246@qq.com>
2022-12-31 11:09:21 +0800

    fixed

--- src/main/java/top/yigumoyan/blogjava/controller/MarkdownController.java
+++ src/main/java/top/yigumoyan/blogjava/controller/MarkdownController.java
@@ -1,5 +1,6 @@
 package top.yigumoyan.blogjava.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -23,7 +24,7 @@ public class MarkdownController {
     private MarkdownServiceImpl markdownService;
 
     @PostMapping("/insertMarkdown")
-    public Result insertMarkdown(@RequestBody Markdown markdown) {
+    public Result<Boolean> insertMarkdown(@RequestBody Markdown markdown) {
         markdownService.insertMarkdown(markdown);
         return Result.ok(true);
     }
@@ -35,39 +36,54 @@ public class MarkdownController {
     }
 
     @PostMapping("/updateMarkdown")
-    public Result updateMarkdown(@RequestBody Markdown markdown) {
+    public Result<Boolean> updateMarkdown(@RequestBody Markdown markdown) {
         markdownService.updateMarkdownById(markdown);
         return Result.ok(true);
     }
 
     @GetMapping("/selectAllMarkdown")
-    public Result selectAllMarkdown() {
+    public Result<List<Markdown>> selectAllMarkdown() {
         return Result.ok(markdownService.selectAllMarkdown());
     }
 
     @GetMapping("/selectAllMarkdownMessage")
-    public Result selectAllMarkdownMessage() {
+    public Result<List<Markdown>> selectAllMarkdownMessage() {
         return Result.ok(markdownService.selectAllMarkdownMessage());
     }
 
     @PostMapping("/selectMarkdownContentById")
-    public Result selectMarkdownContentById(@RequestBody JSONObject json) {
+    public Result<Markdown> selectMarkdownContentById(@RequestBody JSONObject json) {
         return Result.ok(markdownService.selectMarkdownContentById(Integer.parseInt(json.getString("id"))));
     }
 
     @PostMapping("/selectMarkdownByClassification")
-    public Result selectMarkdownByClassification(@RequestBody JSONObject json) {
-        return Result.ok(markdownService.selectMarkdownByClassification(json.getString("label")));
+    public Result<List<Markdown>> selectMarkdownByClassification(@RequestBody JSONObject json) {
+        return Result.ok(markdownService.selectMarkdownByClassification(json.getString("tag")));
+    }
+
+
+    @PostMapping("/selectMarkdownByClassifications")
+    public Result selectMarkdownByClassifications(@RequestBody JSONObject json) {
+        List<String> tagList = (List<String>) json.get("tags");
+        List<Markdown> markdownList = new ArrayList<>();
+        for (String tag : tagList) {
+            for (Markdown markdown : markdownService.selectMarkdownByClassification(tag)) {
+                if (!markdownList.contains(markdown)) {
+                    markdownList.add(markdown);
+                }
+            }
+        }
+        return Result.ok(markdownList);
     }
 
     @PostMapping("/selectMarkdownById")
-    public Result selectMarkdownById(@RequestBody JSONObject json) {
-        System.out.println(json.get("id"));
+    public Result<Markdown> selectMarkdownById(@RequestBody JSONObject json) {
+        System.out.println();
         return Result.ok(markdownService.selectMarkdownById(Integer.parseInt(json.getString("id"))));
     }
 
     @GetMapping("/selectAllClassification")
-    public Result selectAllClassification() {
+    public Result<List<String>> selectAllClassification() {
         List<String> strings = markdownService.selectAllClassification();
         List<String> classificationList = new ArrayList<>();
         for (String str : strings) {
